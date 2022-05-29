09650417897f0aca674338323616af7c29cd0792
Artem <karlin.koleman@bk.ru>
2022-05-29 21:18:32 +0500

    adding dist

---
+++ assets/TheProfile.40ac71dc.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c,e as t}from"./index.1f5212d9.js";const r={},s=t("h1",null,"\u041F\u0440\u043E\u0444\u0438\u043B\u044C\u0430",-1),n=[s];function _(a,l){return o(),c("div",null,n)}var f=e(r,[["render",_]]);export{f as default};

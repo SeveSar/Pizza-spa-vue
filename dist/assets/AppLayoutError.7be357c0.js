8da3b389e94393d36b49eb5393634453fbda5519
Аверин Артём <averin.dev@mail.ru>
2022-11-07 21:25:35 +0500

    Adding dist

--- dist/assets/AppLayoutError.7be357c0.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as r,e as t}from"./index.ec677b75.js";const c={},s=t("h1",null,"ERROR LAYOU",-1),a=[s];function n(_,l){return o(),r("div",null,a)}var f=e(c,[["render",n]]);export{f as default};
